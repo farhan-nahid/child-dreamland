176773f0b4926dd3ccfa950bc1e452abb7831d55
gesora96 <nahidahmed9933@gmail.com>
2022-01-23 02:03:17 +0600

    hero slider done

--- src/components/HomeComponents/HeroSlider/HeroSlider.js
+++ src/components/HomeComponents/HeroSlider/HeroSlider.js
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { AiOutlineStar } from 'react-icons/ai';
+import { BiRocket } from 'react-icons/bi';
+import { BsTrophy } from 'react-icons/bs';
+import { GrLineChart } from 'react-icons/gr';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 import './HeroSlider.scss';
 
-const HeroSlider = () => {
-  return (
-    <div>
-      <h1>Slider</h1>
-    </div>
-  );
-};
+export default class HeroSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      speed: 800,
+      cssEase: 'linear',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      pauseOnHover: false,
+    };
 
-export default HeroSlider;
+    return (
+      <section id='hero__slider'>
+        <Slider {...settings}>
+          <div className='hero__slide one'>
+            <h2>
+              <AiOutlineStar size='2rem' /> <span>WE ARE BEST</span> <AiOutlineStar size='2rem' />
+            </h2>
+            <h1>
+              LET YOUR CHILD <GrLineChart /> <strong>Grow</strong>
+            </h1>
+            <p>
+              We here at <strong>PATHSHALA</strong> provides best education to your little one
+            </p>
+            <button>Know More</button>
+          </div>
+          <div className='hero__slide two'>
+            <h2>
+              <AiOutlineStar size='2rem' /> <span>WE ARE BEST</span> <AiOutlineStar size='2rem' />
+            </h2>
+            <h1>
+              GIVE <BiRocket /> <strong>BOOST</strong> TO YOUR CHILD
+            </h1>
+            <p>
+              We here at <strong>PATHSHALA</strong> provides best education to your little one
+            </p>
+            <button>Know More</button>
+          </div>
+          <div className='hero__slide three'>
+            <h2>
+              <AiOutlineStar size='2rem' /> <span>WE ARE BEST</span> <AiOutlineStar size='2rem' />
+            </h2>
+            <h1>
+              CHOOSE <BsTrophy /> <strong>BEST</strong> FOR YOUR CHILD
+            </h1>
+            <p>
+              We here at <strong>PATHSHALA</strong> provides best education to your little one
+            </p>
+            <button>Know More</button>
+          </div>
+        </Slider>
+      </section>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import 'slick-carousel/slick/slick-theme.css';
-import 'slick-carousel/slick/slick.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
