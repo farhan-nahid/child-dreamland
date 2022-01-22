bec673b181c3a6cd92202d1885449f10cfe32b2c
gesora96 <nahidahmed9933@gmail.com>
2022-01-22 20:17:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+  return <div></div>;
+}
+
+export default App;
+
+/* 
+
+ <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,9 +16,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+*/
