03f89c9f0d430cb6eb64b7e7c7ceaca9bc75584d
gesora96 <nahidahmed9933@gmail.com>
2022-01-23 16:46:06 +0600

    footer done

---
+++ src/components/SharedComponents/Footer/Footer.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { FiFacebook, FiGithub, FiInstagram, FiTwitter } from 'react-icons/fi';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='container'>
+        <h5> PATHSHALA</h5>
+        <h6>BETTER WAY TO LEARN & GROW</h6>
+        <div className='footer__container'>
+          <div className='footer__item'>
+            <h6>CONTACT US</h6>
+            <ul>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  3768 Seabury Ct, Burlington, NC, 27215
+                </a>
+              </li>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  +1 8910000891
+                </a>
+              </li>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  email@pathshala.com
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='footer__item'>
+            <h6>QUICK LINKS</h6>
+            <ul>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  Academics
+                </a>
+              </li>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  Admission
+                </a>
+              </li>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  Events
+                </a>
+              </li>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  Campus Life
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='footer__item'>
+            <h6>SCHOOL TIMINGS</h6>
+            <ul>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  MON - FRI 9:00 AM - 3:00 PM
+                </a>
+              </li>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  SAT 9:00 AM - 1:00 PM
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='footer__item'>
+            <h6>CONTACT US</h6>
+            <ul>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  3768 Seabury Ct, Burlington, NC, 27215
+                </a>
+              </li>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  +1 8910000891
+                </a>
+              </li>
+              <li>
+                <a href='/' target='_blank' rel='noopener noreferrer'>
+                  email@pathshala.com
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='social__media'>
+          <ul>
+            <li>
+              <a href='https://www.facebook.com/dev.farhanNahid' target='_blank' rel='noopener noreferrer'>
+                <FiFacebook />
+              </a>
+            </li>
+            <li>
+              <a href='https://twitter.com/farhan__nahid' target='_blank' rel='noopener noreferrer'>
+                <FiTwitter />
+              </a>
+            </li>
+            <li>
+              <a href='https://github.com/farhan-nahid' target='_blank' rel='noopener noreferrer'>
+                <FiGithub />
+              </a>
+            </li>
+            <li>
+              <a href='https://www.instagram.com/farhan__nahid/' target='_blank' rel='noopener noreferrer'>
+                <FiInstagram />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroSlider from '../components/HomeComponents/HeroSlider/HeroSlider';
 import OurPrincipal from '../components/HomeComponents/OurPrincipal/OurPrincipal';
+import Footer from '../components/SharedComponents/Footer/Footer';
 import Navigation from '../components/SharedComponents/Navigation/Navigation';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <Navigation />
       <HeroSlider />
       <OurPrincipal />
+      <Footer />
     </>
   );
 };
